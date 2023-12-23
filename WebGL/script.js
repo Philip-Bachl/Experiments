7523f58077542151a2fa31fcc83565fc9b8fc80b
Philip-Bachl <p.bachl@icloud.com>
2023-12-23 09:49:29 +0100

    renders triangles now

--- WebGL/script.js
+++ WebGL/script.js
@@ -2,36 +2,54 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("webgl2");
 
 //SOLVE AWAIT PROBLEM
-const vertShaderText = await getShaderText("vert");
-const fragShaderText = await getShaderText("frag");
+let vertShaderText;
+let fragShaderText;
 
-const vertexShader = buildShader(ctx, ctx.VERTEX_SHADER, vertShaderText);
-const fragmentShader = buildShader(ctx, ctx.FRAGMENT_SHADER, fragShaderText);
-const shaderProgram = buildProgram(ctx, vertexShader, fragmentShader);
+let vertexShader;
+let fragmentShader;
+let shaderProgram;
 
-const posAttrLocation = ctx.getAttribLocation(shaderProgram, "a_position");
-const posBuffer = ctx.createBuffer();
+let posAttrLocation;
 
-ctx.bindBuffer(ctx.ARRAY_BUFFER, posBuffer);
+setup();
 
+const positionsStride = 2;
 const positions = [
     0, 0,
     0.2, 0.5,
     0.6, 0
 ];
 
-ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(positions), ctx.STATIC_DRAW);
+const posBuffer = ctx.createBuffer();
+ctx.bindBuffer(ctx.ARRAY_BUFFER, posBuffer);
 
-requestAnimationFrame(update);
+ctx.clearColor(0, 0, 0, 0);
+
+async function setup() {
+    vertShaderText = await getShaderText("vert");
+    fragShaderText = await getShaderText("frag");
+
+    vertexShader = buildShader(ctx, ctx.VERTEX_SHADER, vertShaderText);
+    fragmentShader = buildShader(ctx, ctx.FRAGMENT_SHADER, fragShaderText);
+    shaderProgram = buildProgram(ctx, vertexShader, fragmentShader);
+
+    posAttrLocation =  ctx.getAttribLocation(shaderProgram, "a_position");
+    ctx.vertexAttribPointer(posAttrLocation, 2, ctx.FLOAT, false, 0, 0);
+
+    requestAnimationFrame(update);
+}
 
 function update(now) {
     ctx.viewport(0, 0, canvas.width, canvas.height);
-
-    ctx.clearColor(0, 0, 0, 0);
     ctx.clear(ctx.COLOR_BUFFER_BIT);
+
+    ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(positions), ctx.STATIC_DRAW);
 
     ctx.useProgram(shaderProgram);
     ctx.enableVertexAttribArray(posAttrLocation);
+
+    ctx.drawArrays(ctx.TRIANGLES, 0, positions.length / positionsStride);
+    requestAnimationFrame(update);
 }
 
 function buildProgram(ctx, vertShader, fragShader) {
