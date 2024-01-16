92aabfad710a7924261b004d57e389ef1e4792b2
Philip-Bachl <p.bachl@icloud.com>
2024-01-16 11:45:38 +0100

    Added rustmentation

---
+++ rustmentation/target/debug/deps/rustmentation-9cc59f153628e83f.d
@@ -0,0 +1,5 @@
+C:\Users\Philip\Desktop\Experiments\rustmentation\target\debug\deps\rustmentation-9cc59f153628e83f.rmeta: src\main.rs
+
+C:\Users\Philip\Desktop\Experiments\rustmentation\target\debug\deps\rustmentation-9cc59f153628e83f.d: src\main.rs
+
+src\main.rs:
