5d75c41d22596331a3fcbb9b3230b0a766bbfe79
Philip-Bachl <p.bachl@icloud.com>
2024-01-26 12:21:01 +0100

    added .gitignore

--- rustmentation/target/debug/deps/rustmentation-9cc59f153628e83f.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Philip\Desktop\Experiments\rustmentation\target\debug\deps\rustmentation-9cc59f153628e83f.rmeta: src\main.rs
-
-C:\Users\Philip\Desktop\Experiments\rustmentation\target\debug\deps\rustmentation-9cc59f153628e83f.d: src\main.rs
-
-src\main.rs:
