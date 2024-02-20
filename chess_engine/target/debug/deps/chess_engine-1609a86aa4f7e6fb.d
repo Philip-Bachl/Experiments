746fd5475fabb41498eb940edb03987c944dd2eb
Philip-Bachl <p.bachl@icloud.com>
2024-02-20 08:39:08 +0100

    .gitignore added

--- chess_engine/target/debug/deps/chess_engine-1609a86aa4f7e6fb.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Philip\Desktop\Experiments\chess_engine\target\debug\deps\libchess_engine-1609a86aa4f7e6fb.rmeta: src\main.rs src\board.rs src\piece.rs
-
-C:\Users\Philip\Desktop\Experiments\chess_engine\target\debug\deps\chess_engine-1609a86aa4f7e6fb.d: src\main.rs src\board.rs src\piece.rs
-
-src\main.rs:
-src\board.rs:
-src\piece.rs:
