0c670ebabf0115852cf1f389674e542e06691230
Philip-Bachl <p.bachl@icloud.com>
2024-02-20 12:36:08 +0100

    restructuring

--- chess_engine/src/board.rs
+++ chess_engine/src/board.rs
@@ -1,201 +1,5 @@
-use crate::piece::File;
 use crate::piece::Piece;
 use crate::piece::PieceType;
-
-pub const PIECES: [Piece; 32] = [
-    Piece {
-        is_white: true,
-        piece_type: PieceType::ROOK,
-        file: File::A,
-        rank: 1,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::KNIGHT,
-        file: File::B,
-        rank: 1,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::BISHOP,
-        file: File::C,
-        rank: 1,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::QUEEN,
-        file: File::D,
-        rank: 1,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::KING,
-        file: File::E,
-        rank: 1,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::BISHOP,
-        file: File::F,
-        rank: 1,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::KNIGHT,
-        file: File::G,
-        rank: 1,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::ROOK,
-        file: File::H,
-        rank: 1,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::PAWN,
-        file: File::A,
-        rank: 2,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::PAWN,
-        file: File::B,
-        rank: 2,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::PAWN,
-        file: File::C,
-        rank: 2,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::PAWN,
-        file: File::D,
-        rank: 2,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::PAWN,
-        file: File::E,
-        rank: 2,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::PAWN,
-        file: File::F,
-        rank: 2,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::PAWN,
-        file: File::G,
-        rank: 2,
-    },
-    Piece {
-        is_white: true,
-        piece_type: PieceType::PAWN,
-        file: File::H,
-        rank: 2,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::ROOK,
-        file: File::A,
-        rank: 8,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::KNIGHT,
-        file: File::B,
-        rank: 8,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::BISHOP,
-        file: File::C,
-        rank: 8,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::QUEEN,
-        file: File::D,
-        rank: 8,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::KING,
-        file: File::E,
-        rank: 8,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::BISHOP,
-        file: File::F,
-        rank: 8,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::KNIGHT,
-        file: File::G,
-        rank: 8,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::ROOK,
-        file: File::H,
-        rank: 8,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::PAWN,
-        file: File::A,
-        rank: 7,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::PAWN,
-        file: File::B,
-        rank: 7,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::PAWN,
-        file: File::C,
-        rank: 7,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::PAWN,
-        file: File::D,
-        rank: 7,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::PAWN,
-        file: File::E,
-        rank: 7,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::PAWN,
-        file: File::F,
-        rank: 7,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::PAWN,
-        file: File::G,
-        rank: 7,
-    },
-    Piece {
-        is_white: false,
-        piece_type: PieceType::PAWN,
-        file: File::H,
-        rank: 7,
-    },
-];
 
 pub struct Board {
     pub pieces: Vec<Piece>,
@@ -214,3 +18,198 @@ impl Board {
         }
     }
 }
+
+pub const PIECES: [Piece; 32] = [
+    Piece {
+        is_white: true,
+        piece_type: PieceType::ROOK,
+        file: 0,
+        rank: 0,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::KNIGHT,
+        file: 1,
+        rank: 0,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::BISHOP,
+        file: 2,
+        rank: 0,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::QUEEN,
+        file: 3,
+        rank: 0,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::KING,
+        file: 4,
+        rank: 0,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::BISHOP,
+        file: 5,
+        rank: 0,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::KNIGHT,
+        file: 6,
+        rank: 0,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::ROOK,
+        file: 7,
+        rank: 0,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::PAWN,
+        file: 0,
+        rank: 1,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::PAWN,
+        file: 1,
+        rank: 1,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::PAWN,
+        file: 2,
+        rank: 1,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::PAWN,
+        file: 3,
+        rank: 1,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::PAWN,
+        file: 4,
+        rank: 1,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::PAWN,
+        file: 5,
+        rank: 1,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::PAWN,
+        file: 6,
+        rank: 1,
+    },
+    Piece {
+        is_white: true,
+        piece_type: PieceType::PAWN,
+        file: 7,
+        rank: 1,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::ROOK,
+        file: 0,
+        rank: 7,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::KNIGHT,
+        file: 1,
+        rank: 7,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::BISHOP,
+        file: 2,
+        rank: 7,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::QUEEN,
+        file: 3,
+        rank: 7,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::KING,
+        file: 4,
+        rank: 7,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::BISHOP,
+        file: 5,
+        rank: 7,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::KNIGHT,
+        file: 6,
+        rank: 7,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::ROOK,
+        file: 7,
+        rank: 7,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::PAWN,
+        file: 0,
+        rank: 6,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::PAWN,
+        file: 1,
+        rank: 6,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::PAWN,
+        file: 2,
+        rank: 6,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::PAWN,
+        file: 3,
+        rank: 6,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::PAWN,
+        file: 4,
+        rank: 6,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::PAWN,
+        file: 5,
+        rank: 6,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::PAWN,
+        file: 6,
+        rank: 6,
+    },
+    Piece {
+        is_white: false,
+        piece_type: PieceType::PAWN,
+        file: 7,
+        rank: 6,
+    },
+];
--- chess_engine/src/piece.rs
+++ chess_engine/src/piece.rs
@@ -9,31 +9,9 @@ pub enum PieceType {
 }
 
 #[derive(Clone)]
-pub enum File {
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    G,
-    H,
-}
-
 pub struct Piece {
     pub is_white: bool,
     pub piece_type: PieceType,
-    pub file: File,
+    pub file: u8,
     pub rank: u8,
-}
-
-impl Clone for Piece {
-    fn clone(&self) -> Self {
-        Self {
-            is_white: self.is_white,
-            piece_type: self.piece_type.clone(),
-            file: self.file.clone(),
-            rank: self.rank,
-        }
-    }
 }
