06eb071eec5c04b8b12f83c173cc541681a3c59b
Philip-Bachl <p.bachl@icloud.com>
2024-02-20 12:04:20 +0100

    reorganized older projects

--- lines_to_the_moon/target/debug/deps/lines_to_the_moon-008eca4e6e30d2a9.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Philip\Desktop\Experiments\lines_to_the_moon\target\debug\deps\liblines_to_the_moon-008eca4e6e30d2a9.rmeta: src\main.rs src\screen.rs
-
-C:\Users\Philip\Desktop\Experiments\lines_to_the_moon\target\debug\deps\lines_to_the_moon-008eca4e6e30d2a9.d: src\main.rs src\screen.rs
-
-src\main.rs:
-src\screen.rs:
