1b96db496275d739e4df98a8c1ceb412cb5a3429
Philip-Bachl <p.bachl@icloud.com>
2024-04-19 20:21:18 +0200

    window now closes when pressing the "X"

--- wgpu-testing/src/main.rs
+++ wgpu-testing/src/main.rs
@@ -1,3 +1,32 @@
+use std::{ops::ControlFlow, process::exit};
+
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{self, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let result = event_loop.run(move |event, event_loop| {
+        if let Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            window_id,
+        } = event
+        {
+            if window_id != window.id() {
+                return;
+            }
+            event_loop.exit();
+        }
+    });
+
+    if let Ok(_) = result {
+        return;
+    }
+
+    exit(1);
 }
