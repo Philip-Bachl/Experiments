fdd5ac580a6d57ce650348b04c882d97040d58f7
Philip-Bachl <p.bachl@icloud.com>
2024-04-20 08:09:12 +0200

    reverted to an older version of winit
    also implemented the gpu adapter

--- wgpu-testing/src/main.rs
+++ wgpu-testing/src/main.rs
@@ -1,32 +1,38 @@
-use std::{ops::ControlFlow, process::exit};
-
+use wgpu::{Instance, InstanceDescriptor, PowerPreference, RequestAdapterOptions};
 use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{self, EventLoop},
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 
-fn main() {
+pub fn main() {
     env_logger::init();
-    let event_loop = EventLoop::new().unwrap();
+    let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    let result = event_loop.run(move |event, event_loop| {
-        if let Event::WindowEvent {
-            event: WindowEvent::CloseRequested,
+    let wgpu = Instance::new(InstanceDescriptor::default());
+    let surface = unsafe { wgpu.create_surface(&window).unwrap() };
+
+    let adapter = pollster::block_on(wgpu.request_adapter(&RequestAdapterOptions {
+        power_preference: PowerPreference::default(),
+        compatible_surface: Some(&surface),
+        force_fallback_adapter: false,
+    }));
+
+    /*let adapter = wgpu.request_adapter(&RequestAdapterOptions {
+        power_preference: PowerPreference::default(),
+        compatible_surface: Some(&surface),
+        force_fallback_adapter: false,
+    });*/
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
             window_id,
-        } = event
-        {
-            if window_id != window.id() {
-                return;
-            }
-            event_loop.exit();
-        }
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        _ => {}
     });
-
-    if let Ok(_) = result {
-        return;
-    }
-
-    exit(1);
 }
