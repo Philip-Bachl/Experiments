06db03daec3602cd4d3abbfbb5858422100e3e8f
Philip-Bachl <p.bachl@icloud.com>
2024-05-18 19:47:45 +0200

    added dragging project

---
+++ dragging/script.js
@@ -0,0 +1,21 @@
+const draggables = document.querySelectorAll(".draggable");
+const relativePos = [0, 0];
+
+let grabbed = null;
+
+draggables.forEach(d => {
+    d.addEventListener("mousedown", e => {
+        grabbed = d;
+        relativePos[0] = d.getBoundingClientRect().x - e.clientX;
+        relativePos[1] = d.getBoundingClientRect().y - e.clientY;
+    });
+    d.addEventListener("mouseup", e => {
+        if (d === grabbed) grabbed = null;
+    });
+});
+
+document.addEventListener("mousemove", e => {
+    if (!grabbed) return;
+    grabbed.style.left = (e.clientX + relativePos[0]) + "px";
+    grabbed.style.top = (e.clientY + relativePos[1]) + "px";
+})
