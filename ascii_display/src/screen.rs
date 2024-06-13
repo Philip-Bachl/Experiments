5da5a56542abc40a132df9a819366c0d8914844c
Philip-Bachl <p.bachl@icloud.com>
2024-06-13 06:23:04 +0200

    added lerp + partial draw_line

--- ascii_display/src/screen.rs
+++ ascii_display/src/screen.rs
@@ -1,4 +1,8 @@
-use std::char;
+use core::f64;
+use std::{
+    char,
+    ops::{Add, Mul, Sub},
+};
 
 pub struct Resolution {
     pub width: usize,
@@ -25,11 +29,45 @@ impl Screen {
         &self.resolution
     }
 
-    pub fn draw_fragment(&mut self, x: usize, y: usize, char: char) {
-        self.buffer[x + y * self.resolution.width] = char;
+    pub fn draw_fragment(&mut self, pos: (usize, usize), char: char) {
+        self.buffer[pos.0 + pos.1 * self.resolution.width] = char;
     }
 
-    pub fn draw_line(&mut self, start_x: usize, start_y: usize, end_x: usize, end_y: usize) {
+    /*
+        start, end
+        f(x) = kx + d
+        k = (end.1 - start.1) / (end.0 - start.0)
+        start.1 = d + start.0 * (end.1 - start.1) / (end.0 - start.0)
+        d = start.1 + start.0 * (start.1 - end.1) / (end.0 - start.0)
+    */
+
+    const PRECISION_EXPONENT: u32 = 1;
+    const PRECISION: u32 = 10_u32.pow(Self::PRECISION_EXPONENT);
+    pub fn draw_line(&mut self, start: (usize, usize), end: (usize, usize)) {
+        let delta_x: f64 = end.0 as f64 - start.0 as f64;
+        let delta_y: f64 = end.1 as f64 - start.1 as f64;
+        let length = delta_x.hypot(delta_y);
+
+        for t in 0..Self::PRECISION {
+            let t: f64 = t.into();
+            let precision: f64 = Self::PRECISION.into();
+            let t: f64 = t / precision;
+
+            let start_f64 = (start.0 as f64, start.1 as f64);
+            let end_f64 = (end.0 as f64, end.1 as f64);
+
+            let pos = lerp_f64_pos(start_f64, end_f64, t);
+            todo!()
+        }
+
         todo!()
     }
+}
+
+fn lerp_f64(start: f64, end: f64, t: f64) -> f64 {
+    start + (end - start) * t
+}
+
+fn lerp_f64_pos(start: (f64, f64), end: (f64, f64), t: f64) -> (f64, f64) {
+    (lerp_f64(start.0, end.0, t), lerp_f64(start.1, end.1, t))
 }
