bcdae3ff1ef024dc664ffedd4b745e3c9bebeb6e
Philip-Bachl <p.bachl@icloud.com>
2024-09-21 09:38:27 +0200

    added testing code

--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -4,4 +4,11 @@ use vector::VectorF32;
 pub mod matrix;
 pub mod vector;
 
-fn main() {}
+fn main() {
+    let vector = VectorF32::new(vec![2.0, 3.0]);
+    let matrix = MatrixF32::new(vec![0.0, -1.0, 1.0, 0.0], 2);
+
+    let result = matrix.transform_vecf32(&vector);
+
+    println!("{result:?}");
+}
