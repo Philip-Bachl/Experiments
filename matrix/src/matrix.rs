44e342cac134c49131a9b4cceadf7ef68ed966c7
Philip-Bachl <p.bachl@icloud.com>
2025-03-03 10:05:09 +0100

    added matrix multiplication

--- matrix/src/matrix.rs
+++ matrix/src/matrix.rs
@@ -1,7 +1,7 @@
 use crate::vector::VectorF32;
 
 pub struct MatrixF32 {
-    pub entries: Vec<VectorF32>,
+    pub columns: Vec<VectorF32>,
 }
 
 impl From<Vec<Vec<f32>>> for MatrixF32 {
@@ -13,24 +13,33 @@ impl From<Vec<Vec<f32>>> for MatrixF32 {
 
 impl MatrixF32 {
     pub fn new(entries: Vec<VectorF32>) -> MatrixF32 {
-        Self { entries }
+        Self { columns: entries }
     }
 
     pub fn get_entry_at(&self, row: usize, column: usize) -> f32 {
-        self.entries[column][row]
+        self.columns[column][row]
     }
 
     pub fn scale(&self, scalar: f32) -> MatrixF32 {
-        let entries = self.entries.iter().map(|v| v.scale(scalar)).collect();
+        let entries = self.columns.iter().map(|v| v.scale(scalar)).collect();
         Self::new(entries)
     }
 
     pub fn transform_vecf32(&self, vector: &VectorF32) -> VectorF32 {
-        let mut result = Vec::with_capacity(self.entries.len());
-        for (index, vec) in self.entries.iter().enumerate() {
+        let mut result = Vec::with_capacity(self.columns.len());
+        for (index, vec) in self.columns.iter().enumerate() {
             result.push(vec.scale(vector[index]));
         }
 
         result.into_iter().sum()
+    }
+
+    pub fn transform_matf32(&self, matrix: &MatrixF32) -> MatrixF32 {
+        let columns = matrix
+            .columns
+            .iter()
+            .map(|v| self.transform_vecf32(v))
+            .collect();
+        MatrixF32::new(columns)
     }
 }
